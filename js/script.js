cdd170aedeacc1bf795b0b01838f85b993c504fe
Aranel9966 <99angelo.dire@gmail.com>
2023-02-21 13:43:12 +0100

    finished script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 /*
--creo una variabile vuota per ogni dato
--chiedi all'utente i dati
+-creo una variabile per ogni dato
+    *nome
+    *cognome
+    *colore
+    *numero
+-chiedi i dati all'utente 
 -scrivi sul documento
 */
+
+let userName=prompt('Qualè il tuo nome?');
+let userSurname=prompt('Qualè il tuo cognome?');
+let prefererColor=prompt('Qualè il tuo colore preferito?');
+const randomNumber=24;
+
+//test in consol
+console.log(userName);
+console.log(userSurname);
+console.log(prefererColor);
+
+//stampa sul documento
+document.writeln(`
+Benvenuto ${userName} ${userSurname} ;<br>
+Colore scelto ${prefererColor} ;<br>
+Numero assegnato ${randomNumber} ;<br>
+`)
+
